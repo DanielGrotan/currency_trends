36ac617fe47d3512a9fb3fa6e94d2616cb5b34d1
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2024-05-24 23:12:12 +0200

    initial commit

---
+++ api.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+import pandas as pd
+import streamlit as st
+from pandas import DataFrame
+
+
+@st.cache_data
+def get_currency_trend(
+    currency_code: str, start_date: date, end_date: date
+) -> DataFrame:
+    url = f"https://data.norges-bank.no/api/data/EXR/B.{currency_code}.NOK.SP?format=csv&startPeriod={start_date}&endPeriod={end_date}&locale=no&bom=include"
+
+    df = pd.read_csv(url, sep=";", decimal=",")
+    df = df[["BASE_CUR", "Basisvaluta", "TIME_PERIOD", "OBS_VALUE"]]
+    df.columns = ["Code", "Currency", "Date", "Exchange Rate"]
+    df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+
+    return df
+
+
+@st.cache_data
+def get_currency_codes() -> list[str]:
+    with open("currency_codes.txt") as f:
+        return f.read().splitlines()
---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+import streamlit as st
+
+from api import get_currency_codes, get_currency_trend
+
+if "today" not in st.session_state:
+    st.session_state.today = date.today()
+
+with st.sidebar:
+    currency_code = st.selectbox("Currency code", get_currency_codes())[:3]
+
+today = st.session_state.today
+
+df = get_currency_trend(currency_code, today.replace(year=today.year - 1), today)
+last_date = df["Date"].iloc[-1]
+formatted_date = f"{last_date.day}.{last_date.month}.{last_date.year}"
+
+st.text(f"Exchange rate for {currency_code} is {df["Exchange Rate"].iloc[-1]} kr as of {formatted_date}.")
+
+st.line_chart(df, x="Date", y="Exchange Rate")
